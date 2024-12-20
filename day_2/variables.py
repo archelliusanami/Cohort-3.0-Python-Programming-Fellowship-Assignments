14844d39d54772e44549a91baf6e514a2b26c9d4
Archellius Anami <archelliusanami@gmail.com>
2024-12-21 00:34:03 +0300

    Add day2 folder to the repository

---
+++ day_2/variables.py
@@ -0,0 +1,56 @@
+# Day 2:30 Days of python programming
+first_name = 'Archellius'
+last_name = 'Anami'
+full_name = 'Archellius Anami'
+country = 'Kenya'
+city = 'Nairobi'
+Age = 33
+Year = 1991
+is_married = False
+is_true = True
+is_light_on = True
+skills = ['HTML', 'CSS', 'JS', 'Python']
+
+#Excercise 2
+#check the data type of all your variables using type() built-in function
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(Age))
+print(type(Year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(skills))
+
+len(first_name)
+len(last_name)
+num_one = 5
+num_two = 4
+variable_total = num_one + num_two
+variable_diff = num_two - num_one
+variable_product = num_one * num_two
+variable_division = num_one / num_two
+variable_remainder = num_two % num_one
+variable_exp = num_one ** num_two
+variable_floor_division = num_one // num_two
+
+#calculate the radius of a circle
+radius = 30
+area_of_circle = 3.142 * radius ** 2
+print(area_of_circle)
+circum_of_circle = 2 * 3.142 * radius
+print(circum_of_circle)
+
+#input function
+first_name = input('What is your first name? ') 
+print(first_name)
+last_name = input('What is your last name? ')
+print(last_name)
+country = input('What country do you come from? ')
+print(country)
+age = input('How old are you? ')
+print(age)
+
